b40aa1effbf0f2b84be4954693fc6d919f567b51
Patryk Bajak <Patryk.Bajak@microsoft.wsei.edu.pl>
2024-05-25 16:14:35 +0200

    added questions

--- src/config/question.js
+++ src/config/question.js
@@ -4,30 +4,55 @@ export const reactQuestionsLessons = [
     id: 1,
     questions: [
       {
-        question: "angielski lekcja test",
-        options: ["OK"],
-        correctAnswer: "OK",
+        question: "My daughter said I should take my umbrella, and _ too. While I was out it started pouring with rain.",
+        options: ["just as well", "just as good", "just as lucky"],
+        correctAnswer: "just as well",
       },
       {
-        question: "2+2",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "4",
+        question: " I can't promise he'll listen to me and change his mind, but I'll _.",
+        options: ["see what I can do", "see that I may do", "see what I will do"],
+        correctAnswer: "see what I can do",
       },
       {
-        question: "2+1",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "3",
+        question: " I moved to New York in 2010, and my life has been great _.",
+        options: ["ever after", "ever since", "ever more"],
+        correctAnswer: "ever since",
       },
       {
-        question: "5-3",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "2",
+        question: "My friends don't believe in life after death. They say there's _ as heaven and hell.",
+        options: ["no true thing", "no real thing", "no such thing"],
+        correctAnswer: "no such thing",
       },
       {
-        question: "2+8",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "10",
+        question: "I know you don't really like basketball, but we got the tickets for free so we _ go.",
+        options: ["should as well", "must as well", "may as well"],
+        correctAnswer: "may as well",
       },
+      {
+        question: "Your family will always be there to help you, _.",
+        options: ["no matter what", "no matter which", "no matter that"],
+        correctAnswer: "no matter what",
+      },
+      {
+        question: " I'm cutting down on alcohol, so _ I'm only drinking light beer.",
+        options: ["from now on", "for now on", "since now on"],
+        correctAnswer: "from now on",
+      },
+      {
+        question: "It's often said that there's a _ between genius and madness.",
+        options: ["a thick line", "a fine line", "a long line"],
+        correctAnswer: "a fine line",
+      },
+      {
+        question: " In the village where I grew up, everyone survived by helping _ through the hard times.",
+        options: ["one other", "one or other", "one another"],
+        correctAnswer: "one another",
+      },
+      {
+        question: "There's only one diving board, but if we _ diving everyone will have plenty of dives.",
+        options: ["take goes", "take turns", "take tries"],
+        correctAnswer: "take turns",
+      }
     ]
   }, 
   {
@@ -35,30 +60,55 @@ export const reactQuestionsLessons = [
     id: 2,
     questions: [
       {
-        question: "angielski lekcja test",
-        options: ["OK"],
-        correctAnswer: "OK",
+        question: "Which is produced by insects?",
+        options: ["fruit", "honey", "poultry"],
+        correctAnswer: "honey",
       },
       {
-        question: "69-66",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "3", 
+        question: "If something is edible, it can be",
+        options: ["very delicious", "easily cooked", "safely eaten"],
+        correctAnswer: "safely eaten", 
       },
       {
-        question: "2+1",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "3",
+        question: "Before the development of agriculture, what did people hunt?",
+        options: ["fruits and seeds", "dairy foods", "animals and birds"],
+        correctAnswer: "animals and birds",
       },
       {
-        question: "5-3",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "2",
+        question: "Before the development of agriculture, what did people gather?",
+        options: ["fruits and seeds", "dairy foods", "animals and birds"],
+        correctAnswer: "fruits and seeds",
       },
       {
-        question: "2+8",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "10",
+        question: "Agriculture developed when people began cultivating",
+        options: ["edible plants", "wild animals", "meat and eggs"],
+        correctAnswer: "edible plants",
       },
+      {
+        question: "Which word could be defined as an edible seed that grows in a long seed pod?",
+        options: ["nut", "bean", "grain"],
+        correctAnswer: "bean",
+      },
+      {
+        question: "What are seeds from grasses such as wheat, rice and millet called?",
+        options: ["food seeds", "grains", "beans"],
+        correctAnswer: "grains",
+      },
+      {
+        question: "What do poultry farmers usually raise?",
+        options: ["eggs", "chickens", "feathers"],
+        correctAnswer: "chickens",
+      },
+      {
+        question: "Which are raised for their milk?",
+        options: ["dairy cows", "dairy foods", "beef cattle"],
+        correctAnswer: "dairy cows",
+      },
+      {
+        question: "Which type of food does not always have a hard outer shell?",
+        options: ["nuts", "seafood", "shellfish"],
+        correctAnswer: "seafood",
+      }
     ]
   },
   {
@@ -66,29 +116,111 @@ export const reactQuestionsLessons = [
     id: 3,
     questions: [
       {
-        question: "German lekcja test",
-        options: ["OK"],
-        correctAnswer: "OK",
+        question: "What is 'to write'?",
+        options: ["Schreiben", "Schlange", "Schule"],
+        correctAnswer: "Schreiben",
       },
       {
-        question: "German",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "3", 
+        question: "What is 'Wednesday'?",
+        options: ["Mittwoch", "Donnerstag", "Mittwock"],
+        correctAnswer: "Mittwoch", 
       },
       {
-        question: "2+1",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "3",
+        question: "Is Sunday the English for 'Samstag'?",
+        options: ["True", "False"],
+        correctAnswer: "False",
       },
       {
-        question: "5-3",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "2",
+        question: "How do you say 6:30?",
+        options: ["Halb Sechs", "Halb Sieben", "Halb Acht"],
+        correctAnswer: "Halb Sieben",
       },
       {
-        question: "2+8",
-        options: ["4", "3", "2", "10"],
-        correctAnswer: "10",
+        question: "How do you say 6:45?",
+        options: ["Viertel vor Sieben", "Viertel nach Sechs", "Viertel vor Sechs"],
+        correctAnswer: "Viertel vor Sieben",
+      },
+      {
+        question: "Is 'toll' the German for 'great'?",
+        options: ["True", "False"],
+        correctAnswer: "True",
+      },
+      {
+        question: "Is 'Großvater' the German for Grandfather?",
+        options: ["True", "False"],
+        correctAnswer: "True",
+      },
+      {
+        question: "How do you say 'It does me sorrow' (i.e.'I'm sorry.')?",
+        options: ["Es tut mir leid", "Er ist nicht sehr gut", "Ich bin sieben Jahre alt"],
+        correctAnswer: "Es tut mir leid",
+      },
+      {
+        question: "The words 'welche' and 'welcher' ask 'Which?' .",
+        options: ["True", "False"],
+        correctAnswer: "True",
+      },
+      {
+        question: "Which of the following is Deutsch for the word 'with'?",
+        options: ["Mit", "Und", "Wie"],
+        correctAnswer: "Mit",
+      },
+    ]
+  },
+
+  {
+    language:"German",
+    id: 4,
+    questions: [
+      {
+        question: "True or False'Tag' is the German for 'day'.",
+        options: ["True", "False"],
+        correctAnswer: "True",
+      },
+      {
+        question: "Select the missing word -Wir haben eine kleine Katze _ ein klein Hund.",
+        options: ["Vor", "Und", "Aber"],
+        correctAnswer: "Und", 
+      },
+      {
+        question: "Which sentence is correct way of asking, 'Will you come with me?'",
+        options: ["Wollen Sie mir kommen mit?", "Wollen Sie kommen mit mir?", "Wollen Sie mit mir kommen?"],
+        correctAnswer: "Wollen Sie mit mir kommen?",
+      },
+      {
+        question: "Translate -Wo wohnst du?",
+        options: ["What's you name?", "Where do you live?", "How are you going?"],
+        correctAnswer: "Where do you live?",
+      },
+      {
+        question: "Which of these words means yesterday in German?",
+        options: ["Morgen", "Heute", "Gestern"],
+        correctAnswer: "Gestern",
+      },
+      {
+        question: "The following sentence is logical -'Ich komme aus Tennis'.",
+        options: ["True", "False"],
+        correctAnswer: "False",
+      },
+      {
+        question: "Wie alt bist du?",
+        options: ["Basket ball", "15", "Pizza"],
+        correctAnswer: "15",
+      },
+      {
+        question: "How do you say 'Please'?",
+        options: ["Bitte", "Danke", "Tolle!"],
+        correctAnswer: "Bitte",
+      },
+      {
+        question: "Wie viel kostet das?",
+        options: ["How cold is it?", "How far is that?", "How much does this cost?"],
+        correctAnswer: "Gestern",
+      },
+      {
+        question: "You could drink 'ein Glas Wasser'. True or false? ",
+        options: ["True", "False"],
+        correctAnswer: "True",
       },
     ]
   }
